471da777c4d777f46ebbec767e272cf09b0f3609
Brandon C <63110859+cbshipt@users.noreply.github.com>
2020-12-14 21:34:29 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+function App(){
+  const [displayTime, setDisplayTime] = React.useState(25 * 60);
+  const [breakTime, setBreakTime] = React.useState(5 * 60);
+  const [sessionTime, setSessionTime] = React.useState(25 * 60);
+  const [timerOn, setTimerOn] = React.useState(false);
+  const [onBreak, setOnBreak] = React.useState(false);
+  const [breakAudio, setBreakAudio] = React.useState( new Audio("./pomo.mp3"));
+
+  const playFinishedAudio = () => {
+    breakAudio.currentTime = 0;
+    breakAudio.play();
+  }
+ 
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60); 
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
+
+  const changeTime = (amount, type) => {
+    if (type == "break") {
+      if (breakTime <= 60 && amount < 0 ) {
+        return;
+      }
+      setBreakTime((prev) => prev + amount);
+    } else {
+        if (sessionTime <= 60 && amount < 0) {
+        return;
+      }
+      setSessionTime((prev) => prev + amount);
+      if(!timerOn){
+        setDisplayTime(sessionTime + amount); 
+      }
+    }
+};
+  const controlTime = () => {
+    let second = 1000;
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+    if(!timerOn){
+      let interval = setInterval(() => {
+        date = new Date().getTime();
+        if(date > nextDate){
+          setDisplayTime(prev => {
+            if(prev <= 0 && !onBreakVariable){
+              playFinishedAudio();
+              onBreakVariable=true;
+              setOnBreak(true)
+              return breakTime; 
+            }else if(prev <=0 && onBreakVariable){
+              playFinishedAudio();
+              onBreakVariable=false;
+              setOnBreak(false)
+              return sessionTime;
+            }
+            return prev - 1;
+          });
+          nextDate += second;
+        }
+      }, 30);
+      localStorage.clear();
+      localStorage.setItem('interval-id', interval)
+    }
+
+    if(timerOn) {
+      clearInterval(localStorage.getItem("interval-id"));
+    }
+    setTimerOn(!timerOn);
+  };
+  const resetTime = () => {
+    setDisplayTime(25*60);
+    setBreakTime(5*60);
+    setSessionTime(25 * 60);
+  }
+  return (
+    <div className="center-align">
+        <h1>{formatTime(displayTime)}</h1>
+        <div className="dual-container">
+      <Length title={"Break"} changeTime={changeTime} type={"break"} time={breakTime} formatTime={formatTime}/>
+      <Length title={"Focus"} changeTime={changeTime} type={"session"} time={sessionTime} formatTime={formatTime}/>
+      </div>
+      <h1></h1>
+      <button className="btn-large btn-flat grey lighten-5" onClick={controlTime}>
+        {timerOn ? (
+          <i className="material-icons">radio_button_checked</i>
+        ) : (
+          <i className="material-icons">radio_button_unchecked</i>
+        )}
+      </button>
+      <button className="btn-large btn-flat grey lighten-5" onClick={resetTime}>
+      <i className="material-icons">swap_vertical_circle</i>
+      </button>
+      </div>
+  );
+}
+
+function Length({title, changeTime, type, time, formatTime}){
+  return (
+  <div>
+    <h3>{title}</h3>
+      <div className="time-sets">
+        <button className="btn-small btn-flat waves-effect grey lighten-5"
+            onClick={() => changeTime(-60, type)}
+            >
+            <i className="material-icons">favorite_border</i>
+        </button>
+        <h3>{formatTime(time)}</h3>
+        <button className="btn-small btn-flat waves-effect grey lighten-5"
+          onClick={() => changeTime(60, type)}
+        >
+            <i className="material-icons">favorite</i>
+        </button>
+        
+      </div>
+    </div>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
